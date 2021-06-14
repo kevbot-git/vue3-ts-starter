d70868de5ebbb7a50868ec2bf471b336f176fc10
Kevin Menhinick <kamenhinick@gmail.com>
2021-06-15 10:28:28 +1200

    Add route chunk example

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import About from '../views/About.vue';
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { name: 'home', path: '/', component: Home },
-    { name: 'about', path: '/about', component: About },
+    { name: 'about', path: '/about', component: defineAsyncComponent(() => import('../views/About.vue')) },
   ],
 });
